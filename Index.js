e7ab71954acf0f9d16edafd43057ba516aed04a4
Sayed <sadatsayed92@gmail.com>
2020-09-24 20:20:14 -0700

    My Web Design Project

---
+++ Index.js
@@ -0,0 +1,8 @@
+function infom() {
+    document.getElementById('forDetails').innerHTML = 'Hello Friend, You Clicked on this Image!!!'
+}
+
+function infomBk() {
+    document.getElementById('forDetails').innerHTML = 'Sacramento, California'
+}
+
